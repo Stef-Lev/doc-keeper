c623ca2fe762a6a1a58c166f170738ecbe5778b8
Stefanos Leventis <sigianas@yahoo.com>
2023-12-19 23:10:15 +0200

    start basic api endpoints

---
+++ pages/api/docs/[id].js
@@ -0,0 +1,56 @@
+import dbConnect from "../../../lib/dbConnect";
+import Doc from "../../../models/Doc";
+
+export default async function handler(req, res) {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET" /* Get a model by its ID */:
+      try {
+        const doc = await Doc.findById(id);
+        if (!doc) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: doc });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    case "PUT" /* Edit a model by its ID */:
+      try {
+        const doc = await Doc.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+        if (!doc) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: doc });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    case "DELETE" /* Delete a model by its ID */:
+      try {
+        const deletedDoc = await Doc.deleteOne({ _id: id });
+        if (!deletedDoc) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: {} });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
---
+++ pages/api/docs/index.js
@@ -0,0 +1,30 @@
+import dbConnect from "../../../lib/dbConnect";
+import Doc from "../../../models/Doc";
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      try {
+        const docs = await Doc.find({});
+        res.status(200).json({ success: true, data: docs });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "POST":
+      try {
+        const doc = await Doc.create(req.body);
+        res.status(201).json({ success: true, data: doc });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
