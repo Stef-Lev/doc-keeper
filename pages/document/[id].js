69a0fe101d319e92bfdac63ac0547bd743c45f5f
Stefanos Leventis <sigianas@yahoo.com>
2023-12-21 23:09:16 +0200

    remove comments

--- pages/document/[id].js
+++ pages/document/[id].js
@@ -21,37 +21,3 @@ const DocViewPage = () => {
 };
 
 export default DocViewPage;
-
-// export async function getServerSideProps(context) {
-//   await dbConnect();
-//   console.log(context.query);
-//   const { id } = context.query;
-
-//   // try {
-//   //   let patch = {};
-
-//   //   const { req } = context;
-//   //   const baseUrl = req.headers.host;
-//   //   const protocol = req.headers["x-forwarded-proto"] || "http";
-//   //   const apiUrl = `${protocol}://${baseUrl}/api/patches/${patchId}`;
-
-//   //   const fetchedData = await getOneMethod(apiUrl);
-
-//   //   if (fetchedData) {
-//   //     patch = fetchedData;
-//   //   }
-
-//   //   return {
-//   //     props: {
-//   //       patch,
-//   //     },
-//   //   };
-//   // } catch (error) {
-//   //   console.log(error);
-//   // }
-//   return {
-//     props: {
-//       name: "STEFANOS",
-//     },
-//   };
-// }
