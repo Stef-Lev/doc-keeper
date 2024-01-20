4e6d6e86f691b053953d47bd61fd053362916a39
Stefanos Leventis <sigianas@yahoo.com>
2024-01-21 00:04:24 +0200

    remove comments

--- helpers/contentGetter.js
+++ helpers/contentGetter.js
@@ -1,5 +1,4 @@
 export const getTitle = (dataItem) => {
-  //   console.log(dataItem.content.blocks);
   const title = dataItem.content.blocks.find(
     (block) => block.type === "header-one"
   ).text;
